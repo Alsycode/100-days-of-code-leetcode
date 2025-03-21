74e61cd43e9480bbc73669fd48c85e5a891d4ede
Alsycode <120390865+Alsycode@users.noreply.github.com>
2025-03-21 21:38:16 +0530

    Update solution.js

--- 001/solution.js
+++ 001/solution.js
@@ -1,1 +1,19 @@
 
+var canPlaceFlowers = function(flowerbed, n) {
+    let length = flowerbed.length;
+    let count  = 0;
+    let i = 0;
+    while(i < length){
+        if(flowerbed[i] === 0){
+            const leftplot = (i === 0) || (flowerbed[i - 1] === 0);
+            const rightplot = (i === (length-1)) || (flowerbed[i + 1] === 0)
+            if(rightplot && leftplot){
+                flowerbed[i] = 1;
+                i++
+                count++
+            }
+        }
+        i++
+    }
+    return count >= n
+};
