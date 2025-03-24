33982619568aace34f4e8e3f103e3e4975a8e15a
Alsycode <120390865+Alsycode@users.noreply.github.com>
2025-03-24 11:36:31 +0530

    Update solution.js

--- 003/solution.js
+++ 003/solution.js
@@ -1,1 +1,8 @@
 
+function reverseWords(s) {
+    // Step 1: Trim leading/trailing spaces and split by spaces
+    const wordsArray = s.trim().split(/\s+/);
+    
+    // Step 2: Reverse the array of words  Join the words with a single space
+ return wordsArray.reverse().join(" ");
+}
