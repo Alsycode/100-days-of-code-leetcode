6e7528eb52337d573bad73ef7e5a5544b1a2a8f5
Alsycode <120390865+Alsycode@users.noreply.github.com>
2025-03-25 23:48:20 +0530

    Update solution.js

--- 004/solution.js
+++ 004/solution.js
@@ -1,1 +1,15 @@
 
+
+var moveZeroes = function(nums) {
+    let left = 0;
+    for (let right = 0; right < nums.length; right++){
+        if(nums[right] !== 0){
+        nums[left] = nums[right];
+        left++;
+        }
+    }
+    for(let i = left;i < nums.length;i++){
+        nums[i] = 0;
+    }
+    return nums
+};
