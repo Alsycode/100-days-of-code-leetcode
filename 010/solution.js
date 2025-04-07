8c9de6bc5a9590972519577b7936cd6278f7073c
Alsycode <120390865+Alsycode@users.noreply.github.com>
2025-04-07 22:08:12 +0530

    Update solution.js

--- 010/solution.js
+++ 010/solution.js
@@ -1,1 +1,16 @@
 
+var findMaxAverage = function(nums, k) {
+    if (k > nums.length) return 0;
+    let currentSum = 0;
+    for(let i = 0; i < k; i++){
+        currentSum += nums[i];
+    }
+    let maxSum = currentSum;
+    for(let i = k;
+    i < nums.length;i++){
+        currentSum -= nums[i - k];
+        currentSum += nums[i];
+        maxSum = Math.max(maxSum, currentSum);
+    }
+    return maxSum / k
+};
