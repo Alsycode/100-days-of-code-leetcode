a824133843d96ced2e86de653f6215b0915806ff
Alsycode <120390865+Alsycode@users.noreply.github.com>
2025-04-09 15:42:37 +0530

    Update solution.js

--- 012/solution.js
+++ 012/solution.js
@@ -1,1 +1,14 @@
 
+
+var pivotIndex = function(nums) {
+    const totalSum = nums.reduce((sum, num) => sum + num, 0);
+    let leftSum  = 0;
+    for(let i = 0; i <  nums.length; i++){
+        const rightSum  = totalSum - nums[i] - leftSum;
+        if(leftSum === rightSum){
+            return i;
+        }
+        leftSum += nums[i];
+    }
+    return -1
+};
