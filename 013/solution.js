925aed1e7be8a65b33f79d81892c0175173a357a
Alsycode <120390865+Alsycode@users.noreply.github.com>
2025-04-10 11:42:45 +0530

    Update solution.js

--- 013/solution.js
+++ 013/solution.js
@@ -1,1 +1,8 @@
 
+var findDifference = function(nums1, nums2) {
+    const set1 = new Set(nums1);
+    const set2 = new Set(nums2);
+    const onlyInSet1 = [...set1].filter((item)=>!set2.has(item));
+    const onlyInSet2 = [...set2].filter((item)=>!set1.has(item));
+    return [onlyInSet1, onlyInSet2];
+};
