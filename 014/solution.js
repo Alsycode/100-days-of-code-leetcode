a740bdfbeaa6299efa6cbc7896925c1cced7ed95
Alsycode <120390865+Alsycode@users.noreply.github.com>
2025-04-11 10:30:13 +0530

    Update solution.js

--- 014/solution.js
+++ 014/solution.js
@@ -1,1 +1,14 @@
 
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+    const freqMap = new Map();
+    for(let i = 0; i < arr.length;i++){
+        freqMap.set(arr[i] , (freqMap.get(arr[i]) || 0) + 1);
+    }
+    const frequencies = [...freqMap.values()];
+    console.log(frequencies)
+   return frequencies.length === new Set(frequencies).size;
+};
