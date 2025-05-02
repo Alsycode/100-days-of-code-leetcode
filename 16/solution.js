5212843af764e8064a05622a4d0ca96cd8e01f1c
Alsycode <120390865+Alsycode@users.noreply.github.com>
2025-05-02 13:24:29 +0530

    Update solution.js

--- 16/solution.js
+++ 16/solution.js
@@ -1,1 +1,27 @@
 
+var longestOnes = function(nums, k) {
+    let left = 0;
+    let zeroCount = 0;
+    let maxLength = 0;
+    
+    // Expand window by moving right pointer
+    for (let right = 0; right < nums.length; right++) {
+        // If we encounter a 0, increment zeroCount
+        if (nums[right] === 0) {
+            zeroCount++;
+        }
+        
+        // If zeroCount exceeds k, shrink window from left
+        while (zeroCount > k) {
+            if (nums[left] === 0) {
+                zeroCount--;
+            }
+            left++;
+        }
+        
+        // Update maxLength with current window size
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+    
+    return maxLength;
+};
